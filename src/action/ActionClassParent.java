b1dd3f35bb17bcb92be24d2336c62c750474e7fc
hsengiv <vigneshbe.tm@gmail.com>
2016-09-16 00:03:02 +0530

    action class common things

---
+++ src/action/ActionClassParent.java
@@ -0,0 +1,25 @@
+package action;
+import com.opensymphony.xwork2.ActionSupport;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+public class ActionClassParent extends ActionSupport implements ServletRequestAware, ServletResponseAware{
+	public HttpServletResponse response;
+	public HttpServletRequest request;
+	
+	@Override
+	public void setServletResponse(HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		 this.response = response;
+	}
+
+	@Override
+	public void setServletRequest(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		this.request = request;
+	}
+
+}
